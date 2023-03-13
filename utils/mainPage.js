1b4714d13f4f6c89f844bcba27fcd7e2853d3367
Jesus <perez.jesus1039@gmail.com>
2023-03-12 19:11:00 -0700

    forgot to add the description to the mainPage.js

--- utils/mainPage.js
+++ utils/mainPage.js
@@ -6,10 +6,10 @@ ${answers.description}
 
 ## Table of Contents
 
+- [Description](#description)
 - [Installation](#installation)
 - [Usage](#usage)
 - [Contributing](#contributing)
-- [Tests](#tests)
 - [Questions](#questions)
 
 ## Installation
