e7582260d466a01c412f8026c170ca9d1d3b2c89
Jesus <perez.jesus1039@gmail.com>
2023-03-12 19:00:55 -0700

    added the inquirer.prompt action to make it work

--- mainPage.js
+++ mainPage.js
@@ -1,3 +1,5 @@
+const { default: inquirer } = require("inquirer");
+
 function generateREADME(answers) {
     return
     ${answers.title}
@@ -21,11 +23,18 @@ function generateREADME(answers) {
    ${answers.usage}
 
    ## License
-   
+
    ${answers.license}
 
    ## Questions
 
    If you have any questions, do not contact me .
-   `
+   `;
 }
+
+inquirer.prompt(questions).then({answers} => {
+    const README = generateREADME(answers);
+    FileSystem.writeFile('README.md', README, (err) =>
+    err ? console.error(err) : console.log('README created!')
+    );
+});
